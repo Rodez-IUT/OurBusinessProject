8ae8e633c28d0397311e9e289d4eb7883cdd506e
Franck Silvestre <franck.silvestre@gmail.com>
2021-04-08 14:07:05 +0200

    WIP 4.1

---
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -0,0 +1,36 @@
+package ourbusinessproject;
+
+import java.util.Date;
+
+public class Partnership {
+    private Date creationDate;
+    private Enterprise enterprise;
+    private Project project;
+
+    public Partnership(long id) {
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+}
